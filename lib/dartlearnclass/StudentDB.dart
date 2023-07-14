bc69675cdfc529076cc8d26efcb49916d84f4907
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-14 17:42:57 +0300

    student datas are refactored

---
+++ lib/dartlearnclass/StudentDB.dart
@@ -0,0 +1,24 @@
+import 'Student.dart';
+
+class StudentDB {
+  final _s1 = Student(no: 200, name: "Ahmet", grade: "4A");
+  final _s2 = Student(no: 100, name: "Gizem", grade: "5A");
+  final _s3 = Student(no: 300, name: "Necati", grade: "3A");
+  var _studentList = <Student>[];
+
+  List<Student> getStudentList() {
+    if (_studentList.isEmpty) {
+      _studentList.add(_s1);
+      _studentList.add(_s2);
+      _studentList.add(_s3);
+    }
+
+    return _studentList;
+  }
+
+  void printStudentList(List<Student> list) {
+    for (var student in list) {
+      print("$student");
+    }
+  }
+}
--- lib/dartmainpackage/SortingObjectMain.dart
+++ lib/dartmainpackage/SortingObjectMain.dart
@@ -1,33 +1,23 @@
 import 'package:dart_demo/dartlearnclass/Student.dart';
+import 'package:dart_demo/dartlearnclass/StudentDB.dart';
 
 void main() {
-  var s1 = Student(no: 200, name: "Ahmet", grade: "4A");
-  var s2 = Student(no: 100, name: "Gizem", grade: "5A");
-  var s3 = Student(no: 300, name: "Necati", grade: "3A");
+  var studentDB = StudentDB();
+  var studentList = studentDB.getStudentList();
 
-  var studentList = <Student>[];
-  studentList.add(s1);
-  studentList.add(s2);
-  studentList.add(s3);
   print("Without Sort : ");
-  printStudentList(studentList);
+  studentDB.printStudentList(studentList);
 
   Comparator<Student> sort1 = (x, y) => x.no.compareTo(y.no);
   Comparator<Student> sort2 = (x, y) => x.name.compareTo(y.name);
   Comparator<Student> sort3 = (x, y) => x.grade.compareTo(y.grade);
   studentList.sort(sort1);
   print("After Sort (no): ");
-  printStudentList(studentList);
+  studentDB.printStudentList(studentList);
   studentList.sort(sort2);
   print("After Sort (name): ");
-  printStudentList(studentList);
+  studentDB.printStudentList(studentList);
   studentList.sort(sort3);
   print("After Sort (grade): ");
-  printStudentList(studentList);
-}
-
-void printStudentList(List<Student> list) {
-  for (var student in list) {
-    print("$student");
-  }
+  studentDB.printStudentList(studentList);
 }
