df626929c6daa0ae9b74f46f2940e548ce6ac6da
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-15 15:25:48 +0300

    Learned how to use Row()

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+
 // to delete all comment  lines --> ctrl + f -->//.* --> delete all comments
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp
+
+  ({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,32 +19,48 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: "Hello Flutter"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage
+
+  ({super.key, required this.title});
+
+  final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
-  void initState() {// when app page starts, this method is excecuted
+  void initState() {
+    // when app page starts, this method is excecuted
     super.initState();
     print("-->App is started");
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Title2"),
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text("Hello"),
+      body: Row(
+        children: [
+          Text("Hello"),
+          Container(width: 80,height: 80,color : Colors.red),
+          Container(width: 50,height: 50,color : Colors.blue),
+          Container(width: 100,height: 100,color : Colors.green),
+
+        ],
       ),
     );
   }
