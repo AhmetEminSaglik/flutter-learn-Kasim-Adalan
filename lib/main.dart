cc9a8d8cc478a29848152f3a22481d2a213960d2
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-15 15:31:16 +0300

    Learned how to use Column()

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp
-
-  ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage
-
-  ({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -46,14 +42,22 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Row(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Container(
+          color: Colors.greenAccent,
+          child: Column(
+            children: [
+              Text("Hello"),
+              Container(width: 80, height: 80, color: Colors.red),
+              Container(width: 50, height: 50, color: Colors.blue),
+              Container(width: 200, height: 200, color: Colors.green),
+            ],
+          ),
+        )
+        /* Row(
         children: [
           Text("Hello"),
           Container(width: 80,height: 80,color : Colors.red),
@@ -61,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(width: 100,height: 100,color : Colors.green),
 
         ],
-      ),
-    );
+      ),*/
+        );
   }
 }
