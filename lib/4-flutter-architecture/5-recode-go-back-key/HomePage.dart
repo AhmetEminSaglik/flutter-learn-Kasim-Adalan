d5b7f2a81445a03ebe45bb2717de19028cf5e59f
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-17 18:52:32 +0300

    Go back keys are refactored

---
+++ lib/4-flutter-architecture/5-recode-go-back-key/HomePage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'PageA.dart';
+
+//https://www.udemy.com/course/flutter-ile-uygulama-gelistirme-kursu-android-ios/learn/lecture/23728414#overview
+// to delete all comment  lines --> ctrl + f -->//.* --> delete all comments
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(title: "Hello Flutter"),
+    );
+  }
+}
+
+class StalessWidgetDemo extends StatelessWidget {
+  const StalessWidgetDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PageA()));
+                  },
+                  child: Text("Go to Page A")),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/4-flutter-architecture/5-recode-go-back-key/PageA.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'PageB.dart';
+
+class PageA extends StatefulWidget {
+  const PageA({Key? key}) : super(key: key);
+
+  @override
+  State<PageA> createState() => _PageAState();
+}
+
+class _PageAState extends State<PageA> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Page A"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PageB()));
+                  },
+                  child: Text("Go to Page B")),
+            ],
+          ),
+        ));
+    ;
+  }
+}
