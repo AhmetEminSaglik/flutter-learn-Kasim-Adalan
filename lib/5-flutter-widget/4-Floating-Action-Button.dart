988c233a67f060204082c79ea02fc74822d1bb14
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-19 18:18:26 +0300

    learned Float Action Button widget and where it must be used

---
+++ lib/5-flutter-widget/4-Floating-Action-Button.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+// to delete all comment  lines --> ctrl + f -->//.* --> delete all comments
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var tfController = TextEditingController();
+  var _counter = 0;
+
+  void _increaseCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Float Button Action"),
+      ),
+      body: Column(
+        children: [
+          Text("counter : $_counter"),
+          TextField(
+            decoration: InputDecoration(hintText: "Type"),
+            keyboardType: TextInputType.number,
+            controller: tfController,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          try {
+            _counter = int.parse(tfController.text);
+          } catch (e) {
+            print("Enter only numbers Error occurd  : $e");
+          }
+          _increaseCounter();
+        },
+        tooltip: "Increase Counter",
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepOrange,
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
