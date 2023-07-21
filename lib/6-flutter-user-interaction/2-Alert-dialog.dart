94f11f7b20abdbb5931cc3a9b2d42d02ab04a64e
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-21 06:37:30 +0300

    learned Alert dialog

---
+++ lib/6-flutter-user-interaction/2-Alert-dialog.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+//https://www.udemy.com/course/flutter-ile-uygulama-gelistirme-kursu-android-ios/learn/lecture/23728946#overview
+// to delete all comment  lines --> ctrl + f -->//.* --> delete all comments
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Alert Dialog"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Header"),
+                            content: Text("content"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    print("Selected 'cancel' ");
+                                  },
+                                  child: Text("Cancel")),
+                              TextButton(
+                                  onPressed: () {
+                                    print("Selected 'ok' ");
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("Ok")),
+                            ],
+                          );
+                        });
+                  },
+                  child: Text("Basic Alert")),
+              ElevatedButton(onPressed: () {} , child: Text("Basic Alert")),
+              ElevatedButton(onPressed: () {}, child: Text("Basic Alert")),
+            ],
+          ),
+        ));
+  }
+}
