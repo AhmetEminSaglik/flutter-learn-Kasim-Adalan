1fb8ee531ff6bc638d721446429d49109ec84475
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-23 11:42:11 +0300

    post/insert method is added

--- lib/10-Json/11-http/2-Post/2-Post.dart
+++ lib/10-Json/11-http/2-Post/2-Post.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'User.dart';
 import 'UserDataResult.dart';
 
+//Post search
 Future<List<User>> getUserById(int userId) async {
   Uri url = Uri.parse("https://jsonplaceholder.typicode.com/users");
   var requestData = {"id": userId};
@@ -12,4 +13,12 @@ Future<List<User>> getUserById(int userId) async {
   var jsonData = json.decode(resp.body);
   List<User> userList = UserDataResult.fromJson(jsonData).userList;
   return userList;
+}
+
+//Post Insert
+Future<void> addUser(String name, String phone) async {
+  Uri url = Uri.parse(".../insert/user");
+  var requestData = {"name": name, "phone": phone};
+  var resp = await http.post(url, body: requestData);
+  print("respond : ${resp.body}");
 }
